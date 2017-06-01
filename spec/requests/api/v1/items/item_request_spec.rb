cd2c3aaa8bf5b095dfaa32313ac9f184c22e904b
jk1dd <portajonk@gmail.com>
2017-06-01 10:54:32 -0600

    add item request spec for building the items api

---
+++ spec/requests/api/v1/items/item_request_spec.rb
@@ -0,0 +1,10 @@
+
+
+require 'rails_helper'
+
+describe 'Item API' do
+  it "gets a list of items" do
+# When I send a GET request to /api/v1/items I receive a 200 JSON response containing all items
+# And each item has an id, name, description, and image_url but not the created_at or updated_at
+  end
+end
