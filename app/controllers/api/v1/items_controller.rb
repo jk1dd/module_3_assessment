d75ec80bfb26909a329a9dadfd839c674fc7e1ee
jk1dd <portajonk@gmail.com>
2017-06-01 11:24:47 -0600

    add index, show, and destroy methods, create is in progress. Testing with Postmand still TODO

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -2,4 +2,22 @@ class Api::V1::ItemsController < ApplicationController
   def index
     render json: Item.all
   end
+
+  def show
+    render json: Item.find(params[:id])
+  end
+
+  def destroy
+    render json: Item.delete(params[:id])
+  end
+
+  def create
+
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:)
+  end
 end
--- spec/requests/api/v1/items/item_request_spec.rb
+++ spec/requests/api/v1/items/item_request_spec.rb
@@ -2,17 +2,55 @@ require 'rails_helper'
 
 describe 'Item API' do
   it "gets a list of items" do
-# When I send a GET request to /api/v1/items I receive a 200 JSON response containing all items
-# And each item has an id, name, description, and image_url but not the created_at or updated_at
-  created_items = create_list(:item, 20)
+    created_items = create_list(:item, 20)
 
-  get '/api/v1/items'
+    get '/api/v1/items'
 
-  expect(response).to be_success
-  items = JSON.parse(response.body)
-# binding.pry
-  expect(items.count).to eq(20)
-  expect(items.first["name"]).to eq(created_items.first.name)
-  expect(items.first["created_at"]).to eq(nil)
+    expect(response).to be_success
+    items = JSON.parse(response.body)
+
+    expect(items.count).to eq(20)
+    expect(items.first["name"]).to eq(created_items.first.name)
+    expect(items.first["created_at"]).to eq(nil)
+  end
+
+  it "gets one item by id" do
+    created_item = create(:item)
+
+    get "/api/v1/items/#{created_item.id}"
+
+    expect(response).to be_success
+    item = JSON.parse(response.body)
+
+    expect(item["id"]).to eq(created_item.id)
+    expect(item["name"]).to eq(created_item.name)
+    expect(item["description"]).to eq(created_item.description)
+    expect(item["image_url"]).to eq(created_item.image_url)
+    expect(item["created_at"]).to eq(nil)
+    expect(item["updated_at"]).to eq(nil)
+  end
+
+  it "deletes an item by id" do
+    item1, item2 = create_list(:item, 2)
+
+    expect(Item.count).to eq(2)
+
+    delete "/api/v1/items/#{item1.id}"
+
+    expect(response).to be_success
+    expect(Item.count).to eq(1)
+  end
+
+  it "creates an item" do
+    item_params = {name: "Water", description: "Earth's most precious resource", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCajC7VN-DohIcJhXYJpqL_rSYPOBwmelEipvw1bBMDaViyiAAocPuPlE"}
+
+    post '/api/v1/items', params: {item: item_params}
+
+    item = Item.last
+
+    expect(response).to be_success
+    expect(Item.count).to eq(1)
+    expect(item.name).to eq("Water")
+    expect(item.description).to eq("Earth's most precious resource")
   end
 end
